1bfdb3f97351d9c26296062334afbe6fb37d0023
meskatabusayed <meskatabusayed@gmail.com>
2023-09-15 23:18:08 +0600

    Fundamentals course section wrap up nicely

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,22 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
+
 import './Cart.css'
 
-const Cart = () => {
+const Cart = ({selectedCourse , remaining , creditTotal }) => {
     return (
         <div>
-            <h2>This is Cart</h2>
+            <h2>Credit Hour Remaining {remaining} hr</h2>
+            <h3>Course Name</h3>
+            {
+                selectedCourse.map(course => (
+                   
+                    <li key={course.id}>{course.title}</li>
+                    
+                ))
+            }
+
+            <h5>Total Credit Hour : {creditTotal}</h5>
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,17 +2,58 @@
 import { useEffect, useState } from 'react';
 import './Home.css'
 import {FaBookmark} from 'react-icons/fa';
+import Cart from '../Cart/Cart';
 
 const Home = () => {
 
     const [allCourse , setAllCourse] = useState([]);
-
+    const [selectedCourse , setSelectedCourse] = useState([]);
+    const [remaining , setRemaining] = useState(0);
+    const [creditTotal , setCreditTotal] = useState(0);
 
     useEffect(() => {
         fetch('data.json')
         .then(res => res.json())
         .then(data => setAllCourse(data))
     } ,[])
+
+
+    const handleSelectCourse = (course) => {
+        const isExist = selectedCourse.find((item) => item.id == course.id);
+
+        let count = course.Credit;
+
+        if(isExist){
+           return alert('Already Booked');
+        }
+        else{
+
+            selectedCourse.forEach(item => {
+                count = count + item.Credit;
+            })
+
+            const totalRemaining = 20 - count;
+            
+            if(count > 20){
+                return alert('Credit end');
+            }
+            else{
+                setCreditTotal(count);
+                if(totalRemaining < 0){
+                    return alert('less zero');
+                }
+            setRemaining(totalRemaining);
+
+            const newSelectedCourse = [...selectedCourse , course];
+            setSelectedCourse(newSelectedCourse);
+            }
+
+            
+        }
+
+        
+    }
+
 
 
 
@@ -34,13 +75,17 @@ const Home = () => {
                         <p>$ Price : {course.Price}</p>
                         <p><FaBookmark  className='icon' ></FaBookmark> Credit : {course.Credit} hr</p>
                     </div>
-                    <button className='card-btn'>Select</button>
+                    <button onClick={() => handleSelectCourse(course)} className='card-btn'>Select</button>
                 </div>
                     ))
                 }
                 </div>
                 <div className="cart">
-                    <h1>This is Cart</h1>
+                <Cart selectedCourse = {selectedCourse } 
+                 remaining = {remaining} creditTotal = {creditTotal}
+                
+                
+                ></Cart>
                 </div>
              </div>
         </div>
